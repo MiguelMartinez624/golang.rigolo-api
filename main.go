d2514771e2f00b268997326ccc9ca8d650ac4061
miguelmartinez624 <miguelesalador@gmail.com>
2021-06-18 23:47:21 -0400

    feat:databse variables

--- main.go
+++ main.go
@@ -11,9 +11,18 @@ import (
 	"os"
 )
 
+const defaultPort = "8080"
+
 func main() {
 	db := createDatabaseConnection()
+
+	serverPort := os.Getenv("PORT")
+	if serverPort == "" {
+		serverPort = defaultPort
+	}
+
 	r := gin.Default()
+
 	r.Use(CORS)
 	r.GET("/", func(context *gin.Context) {
 		context.JSON(200, gin.H{
@@ -29,7 +38,7 @@ func main() {
 	// Context bootstrapping
 	clients.Bootstrap(clients.ClientsServiceConfiguration{DB: db, Server: r})
 
-	log.Fatal(r.Run())
+	log.Fatal(r.Run(fmt.Sprintf(":%v", serverPort)))
 }
 func CORS(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
@@ -44,13 +53,16 @@ func CORS(c *gin.Context) {
 	}
 }
 func createDatabaseConnection() *gorm.DB {
-	username := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASS")
-	dbName := os.Getenv("DB_NAME")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dsn := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", dbHost, username, dbName, password, dbPort)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		username := os.Getenv("DB_USER")
+		password := os.Getenv("DB_PASS")
+		dbName := os.Getenv("DB_NAME")
+		dbHost := os.Getenv("DB_HOST")
+		dbPort := os.Getenv("DB_PORT")
+		databaseURL = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", dbHost, username, dbName, password, dbPort)
+	}
+	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 	if err != nil {
 		log.Println(err.Error())
 		panic("failed to connect database")
