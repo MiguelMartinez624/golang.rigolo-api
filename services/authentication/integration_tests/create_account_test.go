35b50b210d83a000ded426ce15b2e8bbe41657ff
miguelmartinez624 <miguelesalador@gmail.com>
2021-06-25 12:26:34 -0400

    feat:basic test imple

--- services/authentication/integration_tests/create_account_test.go
+++ services/authentication/integration_tests/create_account_test.go
@@ -3,6 +3,7 @@ package integration_tests
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"github.com/bmizerany/assert"
 	"github.com/rigolo-api/common"
 	"github.com/rigolo-api/services/authentication/domain/data"
@@ -12,6 +13,7 @@ import (
 )
 
 func TestCreateAccount(t *testing.T) {
+
 	t.Run("should create account with valid data", func(t *testing.T) {
 		// Given
 		rr := httptest.NewRecorder()
@@ -38,5 +40,34 @@ func TestCreateAccount(t *testing.T) {
 		//expected
 		assert.Equal(t, rr.Body.Bytes(), expectedBytes)
 		assert.Equal(t, 200, rr.Code)
+	})
+
+
+	t.Run("cant create account with invalid email or null", func(t *testing.T) {
+		// Given
+		rr := httptest.NewRecorder()
+		requestStruct := data.NewAccount{
+			Email:     "tester.com",
+			Password:  "test",
+			LastName:  "tester",
+			FirstName: "tesresr",
+			Age:       12,
+		}
+		data, err := json.Marshal(requestStruct)
+		assert.Equal(t, err, nil)
+		requestBody := bytes.NewReader(data)
+		request := httptest.NewRequest(http.MethodPost, "/auth/signup", requestBody)
+
+		// When
+		server.ServeHTTP(rr, request)
+		expectedResponse := common.Result{Error: errors.New("invalid email format"), Data: nil}
+		expectedBytes, err := json.Marshal(&expectedResponse)
+		if err != nil {
+			panic(err)
+		}
+
+		//expected
+		assert.Equal(t, rr.Body.Bytes(), expectedBytes)
+		assert.Equal(t, 400, rr.Code)
 	})
 }
